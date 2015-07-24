b6eac3f812d516c9dc41ec99b3ddc0133aca28c4
alex kochetov <as.kochetov@gmail.com>
2015-07-24 12:26:52 +0300

    Improvement
    No need for hasClass helper - read this: 
    https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
    
    No need for check on your line 15 - nobody is using window event. 
    No need to cache e.target - you're using it only once.
    
    For If condition it's better to invert it and return false if it's not true - you have less indents in this case.

--- app/js/picker.js
+++ app/js/picker.js
@@ -3,20 +3,12 @@
 const toggle = require('./toggle');
 
 let addEvent = require('./events'),
-	picker = document.querySelector('.header__color-picker'),
-	palette = document.querySelector('.header__picker');
-
-let _hasClass = function (el, className) {
-	let regExp = new RegExp(className);
-	return regExp.test(el.className);
-};
+    picker = document.querySelector('.header__color-picker'),
+    palette = document.querySelector('.header__picker');
 
 addEvent(picker, 'click',  function (e) {
-	e = e || window.event;
-	var target = e.target;
-	if (_hasClass(target, 'color-picker')) {
-		toggle(palette, 'active');
-		toggle(picker, 'shadow');
-	}
+    if (!e.target.classList.contains('color-picker')) return false;
+	
+    toggle(palette, 'active');
+    toggle(picker, 'shadow');
 });
-
